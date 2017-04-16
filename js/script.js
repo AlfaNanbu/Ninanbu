839be37f1d5589703344aca0847c3184cfdd6225
Alfacoy <ninanbudevelop@gmail.com>
2017-04-16 18:19:23 -0300

    Update Ninanbu
    Se agregan los archivos BASES de la web al repositorio Ninanbu.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function UnderConstruct()
+{
+  alert("La Pagina se encuentra en construccion por lo cual no esta disponible.")
+}
+
+
+var x= document.getElementById('GuerraAlpha');
+x.addEventListener("click",UnderConstruct);
