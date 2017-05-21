43883e92b630980b38093e05272a0d92d65f2541
Alfacoy <ninanbudevelop@gmail.com>
2017-05-21 01:47:08 -0300

    Ninanbu 2.0
    Se limpia codigo viejo y se agregan clases unificadas.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
+//====== Llamado de Funciones ======
+resetTec();
+
+
+
+//====== Funciones Generales ======
 function UnderConstruct()
 {
   alert("La Pagina se encuentra en construccion por lo cual no esta disponible.")
 }
 
 
-var x= document.getElementById('GuerraAlpha');
-x.addEventListener("click",UnderConstruct);
+// ====== Seccion de Tecnologias ======
+var a = document.querySelector('#front');
+a.addEventListener("click",rotarFront);
+var b = document.querySelector('#back');
+b.addEventListener("click",rotarBack);
+var c = document.querySelector('#frame');
+c.addEventListener("click",rotarFrame);
+
+function resetTec()
+{
+  dinamic2.style.display="none";
+  dinamic3.style.display="none";
+  dinamic1.style.display="none";
+}
+
+function rotarFront()
+{
+  dinamic0.style.display="none";
+  dinamic2.style.display="none";
+  dinamic3.style.display="none";
+  dinamic1.style.display="block";
+}
+
+function rotarBack()
+{
+  dinamic0.style.display="none";
+  dinamic1.style.display="none";
+  dinamic3.style.display="none";
+  dinamic2.style.display="block";
+}
+
+function rotarFrame()
+{
+  dinamic0.style.display="none";
+  dinamic1.style.display="none";
+  dinamic2.style.display="none";
+  dinamic3.style.display="block";
+}
